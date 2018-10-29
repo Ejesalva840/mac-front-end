fa061d2b9375586c75e59895c31953a3f13a10c0
Jesalva, Elijah <ejesalva840@gmail.com>
2018-10-28 21:12:56 -0400

    Close sideNav on sidebar click

--- src/app/navigation/side-bar/side-bar.component.ts
+++ src/app/navigation/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSidenav } from '../../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  @Input() sideBarNavComponent: MatSidenav;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  closeSideNav(): void {
+    this.sideBarNavComponent.close();
   }
 
 }
