1feab0e28712cb8e14f0e6988d2c7abf2088c0a3
Jesalva, Elijah <ejesalva840@gmail.com>
2018-11-05 21:41:02 -0500

    Setup basic search capabilities

--- src/app/navigation/student-details/student-details.component.ts
+++ src/app/navigation/student-details/student-details.component.ts
@@ -2,6 +2,7 @@ import { StudentService } from '../../services/student.service';
 import { Student } from '../../entities/student';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { StudentDetails } from '../../entities/student-details';
 
 @Component({
   selector: 'app-student-details',
@@ -11,6 +12,7 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 export class StudentDetailsComponent implements OnInit {
 
   studentsList: MatTableDataSource<Student>;
+  studentDetails: StudentDetails = null;
 
   columnsToDisplay = ['name', 'belt', 'style'];
 
@@ -24,6 +26,10 @@ export class StudentDetailsComponent implements OnInit {
     this.studentsList = new MatTableDataSource<Student>(this.studentService.getAllUsers());
     this.studentsList.paginator = this.studentPager;
     this.studentsList.sort = this.studentSorter;
+  }
+
+  selectStudent(student: Student) {
+    this.studentDetails = this.studentService.getUserDetails(student.studentId);
   }
 
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,6 +1,8 @@
+import { StudentDetails } from './../entities/student-details';
 import { MOCKSTUDENTS } from '../entities/mock-students';
 import { Injectable, OnInit } from '@angular/core';
 import { Student } from '../entities/student';
+import { MOCKSTUDENTDETAILS } from '../entities/mock-student-details';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,14 @@ import { Student } from '../entities/student';
 export class StudentService implements OnInit {
 
   studentsList: Student[];
+  studentDetailsList: StudentDetails[];
 
   ngOnInit(): void {
   }
 
   constructor() {
     this.studentsList = MOCKSTUDENTS;
+    this.studentDetailsList = MOCKSTUDENTDETAILS;
   }
 
   getAllUsers(): Student[] {
@@ -22,5 +26,9 @@ export class StudentService implements OnInit {
 
   getUserByArt(martialArtStyle: String): Student[] {
     return this.studentsList.filter(user => user.martialStyle.toUpperCase() === martialArtStyle.toUpperCase());
+  }
+
+  getUserDetails(userId: number): StudentDetails {
+    return this.studentDetailsList.find(student => student.studentId === userId);
   }
 }
